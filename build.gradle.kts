5ec3108b888485003223150920e87f315a68779a
Mercafed <vmguevara2007@gmail.com>
2025-11-16 00:02:55 -0500

    Fix merge conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -34,40 +33,3 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
 }
-=======
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-    id("com.android.library")
-    kotlin("android")
-}
-
-android {
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 34
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-repositories {
-    mavenCentral()
-    google()
-    maven(url = "https://repo.cloudstream.cf")
-}
-
-dependencies {
-    implementation("com.lagradost:cloudstream:3.2.0")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-}
->>>>>>> 04bce369166c5bc1859bf136f59a86bcfa8004f9
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 rootProject.name = "MiExtension"
-=======
-rootProject.name = "MiExtension"
->>>>>>> 04bce369166c5bc1859bf136f59a86bcfa8004f9
